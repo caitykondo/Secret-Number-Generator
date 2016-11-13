0914b918ae370b92bbb2185a251f35694531d750
caitypizza <caitykondo@gmail.com>
2016-11-12 14:41:21 -1000

    completed secret number generator

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,18 @@
 'use strict';
-module.exports = function() {
-  
+module.exports = function secretNumber() {
+  let secretNumberGenerator = Math.random() * 100000; //0 (function() {
+    return function num(){
+      return secretNumberGenerator;
+    };
 };
+
+
+// var generatedNumber = secretNumber();
+
+// console.log(generatedNumber);
+
+
+// var anotherSecret = secretNumber();
+// console.log(anotherSecret + ' <-- this should be the same');
+// console.log(anotherSecret + ' <-- as this');
+// console.log(secretNumber.num);
